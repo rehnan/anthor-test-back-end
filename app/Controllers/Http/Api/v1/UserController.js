1bc45efcf95b6003973f24bace79f2a376a12a0e
Rehnan Carolino <rehnancarolino@live.com>
2019-01-16 18:11:25 -0200

    [New] Implement user controller methods and improve it

--- app/Controllers/Http/Api/v1/UserController.js
+++ app/Controllers/Http/Api/v1/UserController.js
@@ -1,35 +1,60 @@
 'use strict'
+const User = use('App/Models/User');
 
 class UserController {
-  async signIn ({ request, response }) {
+  async signIn ({ request, response, auth }) {
     try {
-      throw new Error('Not implemented yet!');
+      const {email, password} = request.all();
+      let user = await User.where({email}).first();
+      if (user) {
+        let data = {};
+        Object.assign(data, {user: user.toJSON()});
+        const attempt = await auth.attempt(user.email, password);
+        Object.assign(data, {token: attempt.token});
+        return response.json(data);
+      }
     } catch (error) {
-      return response.notImplemented({message: 'Not implemented yet!'});
+      // console.error(error.message);
+    }
+    return response.unauthorized({message: 'Invalid credentails!'});
+  }
+
+  async signOut ({ request, response, auth }) {
+    try {
+      const currentUserToken = auth.getAuthHeader();
+      const isLoggedIn = await auth.check();
+      let logout = false;
+      if (isLoggedIn) {
+        await auth.revokeTokens([currentUserToken], true);
+        logout = true;
+      }
+      return response.json({logout});
+    } catch (error) {
+      // console.error(error.message);
+    }
+    return response.unauthorized({message: 'Invalid credentails!'});
+  }
+
+  async isLoggedIn ({ request, response, auth }) {
+    try {
+      const isLoggedIn = await auth.check();
+      return response.json({isLoggedIn});
+    } catch (error) {
+      // console.error(error.message);
+      return response.unauthorized({message: 'Invalid credentails!'});
     }
   }
 
-  async signOut ({ request, response }) {
+  async getUserBy ({ request, response, auth }) {
     try {
-      throw new Error('Not implemented yet!');
+      const isLoggedIn = await auth.check();
+      if (isLoggedIn) {
+        const user = await auth.getUser();
+        return response.json({user});
+      }
     } catch (error) {
-      return response.notImplemented({message: 'Not implemented yet!'});
-    }
-  }
-
-  async getUserBy ({ request, response }) {
-    try {
-      throw new Error('Not implemented yet!');
-    } catch (error) {
-      return response.notImplemented({message: 'Not implemented yet!'});
-    }
-  }
-
-  async isAuthenticated ({ request, response }) {
-    try {
-      throw new Error('Not implemented yet!');
-    } catch (error) {
-      return response.notImplemented({message: 'Not implemented yet!'});
+      // console.error(error.message);
+      return response.unauthorized({message: 'Invalid credentails!'});
     }
   }
 }
