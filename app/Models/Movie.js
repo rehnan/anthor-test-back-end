4698afd00cffcb5a7cedca6b3148098282ababe5
Rehnan Carolino <rehnancarolino@live.com>
2019-01-16 21:56:53 -0200

    [New] Define movie schema and declare methods

--- app/Models/Movie.js
+++ app/Models/Movie.js
@@ -33,6 +33,69 @@ class Movie extends Model {
       'website',
     ];
   }
+
+  /**
+   * Create new movie
+   * @param data - data request params
+   * @returns {Promise<Movie>} - returns new movie instance created
+   */
+  static async createNewMovie(data) {
+    throw new Error('Not implemented yet!');
+  }
+
+  /**
+   * Get movie list
+   * @returns {Promise<Movie>} - returns array of movies
+   */
+  static async getAllMovies() {
+    throw new Error('Not implemented yet!');
+  }
+
+  /**
+   * Get move by filter
+   * @param conditions - query conditions to filter movies
+   * @returns {Promise<Array<Movie>>}
+   */
+  static async findMovieBy(conditions) {
+    throw new Error('Not implemented yet!');
+  }
+
+  /**
+   * Update movie by id
+   * @param id - movie id
+   * @param data - data request params
+   * @returns {Promise<Movie>} - returns movie instance updated
+   */
+  static async updateMovieById(id, data) {
+    throw new Error('Not implemented yet!');
+  }
+
+  /**
+   * Delete movie by id
+   * @param id - movie id
+   * @returns {Promise<Movie>} - returns movie instance removed
+   */
+  static async deleteMovieById(id) {
+    throw new Error('Not implemented yet!');
+  }
+
+  /**
+   * Sanitize data attributes
+   * @param attributes - data attributes that will be sanitized
+   * @returns {Promise<Array<Object>>} - Returns array of data attributes
+   */
+  static async sanitizeAttributes(attributes) {
+    throw new Error('Not implemented yet!');
+  }
+
+  /**
+   * Validate attribute values
+   * @param attributes - data attributes
+   * @returns {Promise<void>}
+   */
+  static async validateAttributeValues(attributes) {
+    throw new Error('Not implemented yet!');
+  }
 }
 
 module.exports = Movie;
