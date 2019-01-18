47dafaf84e66d400715c83389b9fb1a8a71383a2
Rehnan Carolino <rehnancarolino@live.com>
2019-01-18 10:47:54 -0200

    [New] Implement movie controller methods

--- app/Controllers/Http/Api/v1/MovieController.js
+++ app/Controllers/Http/Api/v1/MovieController.js
@@ -4,47 +4,52 @@ const Movie = use('App/Models/Movie');
 class MovieController {
   async getMovies ({ request, response }) {
     try {
-
+      const movies = await Movie.getAllMovies();
+      return response.json({movies});
     } catch (error) {
       // console.error(error.message);
     }
-    return response.unauthorized({message: 'Invalid credentials!'});
+    return response.unauthorized({message: 'Unauthorized!'});
   }
 
-  async getMovieById ({ request, response }) {
+  async getMovieById ({ request, response, params }) {
     try {
-
+      const movie = await Movie.findMovieBy({_id: params.id});
+      return response.json({movie});
     } catch (error) {
       // console.error(error.message);
     }
-    return response.unauthorized({message: 'Invalid credentials!'});
+    return response.unauthorized({message: 'Unauthorized!'});
   }
 
   async createMovie ({ request, response }) {
     try {
-
+      const movie = await Movie.createNewMovie(request.all());
+      return response.json({movie});
     } catch (error) {
       // console.error(error.message);
     }
-    return response.unauthorized({message: 'Invalid credentials!'});
+    return response.unauthorized({message: 'Unauthorized!'});
   }
 
-  async updateMovieById ({ request, response }) {
+  async updateMovieById ({ request, response, params }) {
     try {
-
+      const movie = await Movie.updateMovieById(params.id, request.all());
+      return response.json({movie});
     } catch (error) {
       // console.error(error.message);
     }
-    return response.unauthorized({message: 'Invalid credentials!'});
+    return response.unauthorized({message: 'Unauthorized!'});
   }
 
-  async deleteMovieById ({ request, response }) {
+  async deleteMovieById ({ request, response, params }) {
     try {
-
+      const removed = await Movie.deleteMovieById(params.id);
+      return response.json({wasRemoved: removed});
     } catch (error) {
       // console.error(error.message);
     }
-    return response.unauthorized({message: 'Invalid credentials!'});
+    return response.unauthorized({message: 'Unauthorized!'});
   }
 }
 
